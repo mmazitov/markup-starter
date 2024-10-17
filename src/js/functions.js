1435e871aa8f61a5bb3cf81efe9dde31cbe3132b
mmazitov <mazitov.nikolya@gmail.com>
2024-10-17 14:33:12 +0300

    remove unused

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,12 +1,1 @@
-document.addEventListener('DOMContentLoaded', function () {
-	initSidebarBtn();
-});
-
-function initSidebarBtn() {
-	const resizeBtn = document.querySelector('.resize');
-	const content = document.querySelector('.wrapper');
-	resizeBtn.addEventListener('click', function(e) {
-		e.preventDefault();
-		content.classList.toggle('sb-expand');
-	});
-}
+document.addEventListener('DOMContentLoaded', function () {});
